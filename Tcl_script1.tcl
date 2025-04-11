afbf9dc623486338dd95c0c2c3d6139e6cb76b08
Xiliyohuala <ilovePRC2050@outlook.com>
2025-04-11 19:18:26 +0800

    test

---
+++ Tcl_script1.tcl
@@ -0,0 +1,26 @@
+#Setup pin setting for EP4C6_board
+set_global_assignment -name RESERVE_ALL_UNUSED_PINS "AS INPUT TRI-STATED"
+
+set_location_assignment PIN_23 -to clk
+
+set_location_assignment PIN_143 -to data_out[0]
+set_location_assignment PIN_142 -to data_out[1]
+set_location_assignment PIN_141 -to data_out[2]
+set_location_assignment PIN_138 -to data_out[3]
+set_location_assignment PIN_137 -to data_out[4]
+set_location_assignment PIN_136 -to data_out[5]
+set_location_assignment PIN_135 -to data_out[6]
+
+set_location_assignment PIN_129 -to segcom[0]
+set_location_assignment PIN_3 -to segcom[1]
+set_location_assignment PIN_2 -to segcom[2]
+set_location_assignment PIN_1 -to segcom[3]
+set_location_assignment PIN_144 -to segdot
+
+set_location_assignment PIN_30 -to key1
+set_location_assignment PIN_32 -to key2
+set_location_assignment PIN_34 -to key3
+set_location_assignment PIN_39 -to key4
+
+set_location_assignment PIN_31 -to led2
+
